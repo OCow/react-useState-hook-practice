c02645802b03cd9c2be9f06353d601af5af211f3
oliver.kuhn2@gmail.com <oliver.kuhn2@gmail.com>
2020-11-01 21:23:19 +0100

    showing time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [currentTimeString, btnSetTime] = useState(
+    new Date().toLocaleTimeString()
+  );
+
+  function btnGetTime() {
+    //console.log("Button pressed");
+    btnSetTime(new Date().toLocaleTimeString());
+  }
+
+  // setInterval(() => {
+  //   btnSetTime(new Date().toLocaleTimeString());
+  // }, 1000);
+  setInterval(btnGetTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTimeString}</h1>
+      <button onClick={btnGetTime}>Get Time</button>
     </div>
   );
 }
